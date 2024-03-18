c61ad96eeb4c7dc8eda168c7ffe9d23d02b70625
Marton Csutora <marton@csutora.com>
2024-03-18 17:55:35 +0100

    added test husks

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,10 +4,44 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 
 import static org.mockito.Mockito.*;
 
 
 public class TrainSensorTest {
+	TrainSensor sensor;
+	
+	@Before
+	public void before() {
+        TrainController mockController = mock(TrainController.class);
+        TrainUser mockUser = mock(TrainUser.class);
+		sensor = new TrainSensorImpl(mockController, mockUser);
+        mockController.setSpeedLimit(10);
+        mockUser.overrideJoystickPosition(10);
+        mockController.followSpeed();
+	}
+	
+	@Test
+	public void settingSpeedLimitBelowZero_AlarmsUser() {
+
+	}
+
+    @Test
+	public void settingSpeedLimitAboveFiveHundred_AlarmsUser() {
+
+	}
+
+    @Test
+	public void settingSpeedLimitBelowFiftyPercentReferenceSpeed_AlarmsUser() {
+
+	}
+
+    @Test
+	public void settingSpeedLimitInCorrectRange_DoesntAlarmUser() {
+
+	}
 
 }
